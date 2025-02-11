98da161c34b351c330ecb139ddde86b53b5a12aa
Romii646 <129922665+Romii646@users.noreply.github.com>
2025-02-11 00:43:45 -0500

    Update formProcess.php
    updated form1 to be more dynamic and take in any insert form size without needing to change php functions throughout the code base

--- formProcess.php
+++ formProcess.php
@@ -7,104 +7,83 @@
 </head>
 <body>
     <?php
-    // Created by Aaron C. 09/25/2024 Finished: 12/01/2024
-       error_reporting(E_ALL);
-       ini_set('display_errors', 1);
+    // Start of form processing for keyboard page
+    // Variable list and require list
+    require 'wLInventory.php';
+    require $sOP;
+    require $errorFunctions;
 
-       if($_SERVER["REQUEST_METHOD"] == "POST") {
-          //echo "Form submitted successfully.<br>";
-       } 
-       else {
-        echo "No form data received.<br>";
-       }
-
-    // start of form processing for keyboard page
-    // variable list and require list
-       require 'wLInventory.php';
-       require $sOP;
-       $errorCount = 0;
-       
-       // display error function
-       function display_error($entryName){
-           global $errorCount;
-           ++$errorCount;
-           echo "the field \"$entryName\" is required. Error count: \"$errorCount\"<br>";
-        }
-    // validate input function
-       function validate_input($data, $entryName):String{
-          global $errorCount;
-          if(empty($data)){
-            display_error($entryName);
-            $data = "";
-          }
-          return $data;
-        }
-       // end of function list
-
-       // start of main program
-       // Insert form
-       $form = $_POST['form'];
-       switch($form){
+    // Start of main program
+    // Insert form
+    $form = $_POST['form'];
+    switch($form){
         case 'form1':
-           $insertOP = new insertOp(); // class object
-           // start of assiging variables to POST values
-           $table_name = validate_input($_POST['tableSelect'], $description_k[6]);
-           $p_id_value = validate_input($_POST[$wordList[0]], $description_k[0]);
-           $name = validate_input($_POST[$wordList[1]], $description_k[2]);
-           $condition = validate_input($_POST[$wordList[2]], $description_k[3]);
-           $cost = validate_input($_POST[$wordList[3]], $description_k[4]);
-           if (!is_numeric($cost)) {
-               ++$errorCount;
-               echo "The value for cost is not numeric. Error count: \"$errorCount\"<br>";
-               $cost = null;
-           } 
-           else {
-               $cost = (float)$cost;
-           }
-           $location = validate_input($_POST[$wordList[4]],$description_k[5]);
-           $type = $_POST[$wordList[5]];
-           $monitorSize = $_POST[$wordList[7]];
-           $speed = $_POST[$wordList[8]];
-           $watt = $_POST[$wordList[9]];
-           $mod = $_POST[$wordList[10]];
-           $motherboardSize = $_POST[$wordList[11]];
-           // end of assigning variable to POST values
-           if($errorCount == 0) {
-              $idName = find_ID($table_name);
-              $insertOP -> connect();
-              $insertOP -> set_table_names($idName, $wordList[1], $wordList[2], $wordList[3], $wordList[4],
-                                            $wordList[6], type: $wordList[5], speed: $wordList[8], wattage: $wordList[9], modular: $wordList[10]);
-              $insertOP -> add_query(id_value: $p_id_value, name_value: $name, condition_value: $condition, cost_value: $cost, location_value: $location, monitor_size_value: $monitorSize,
-                                     motherboard_value: $motherboardSize,speed_value: $speed, type_value: $type, wattage_value: $watt, modular_value: $mod,tableName: $table_name);
-              $insertOP -> DB_close();
-              header("Location: inventoryForm.html");
-              exit();
-           }
-           else{
-              echo "No form data recieved";
-           }
-           break;
+            $insertOP = new insertOp(); // Class object for inserting data
+            $columnValues = [];
+            $columnNames = [];
+            $count = 0;
+            // Start of assigning variables to POST values
+
+            foreach($_POST as $post => $values){
+                if($post != "form" &&  $post != "tableSelect" && !empty($values)){
+                    $columnValues[$count] = validate_input($values, $post);
+                    if($post == "cost"){
+                        if (!is_numeric($columnValues[$count])) {
+                            ++$errorCount;
+                            echo "The value for cost is not numeric. Error count: \"$errorCount\"<br>";
+                            $columnValues[$count] = null;
+                        } 
+                        else {
+                            $columnValues[$count] = (float)$columnValues[$count];
+                        }
+                    }
+                    if($post != "p_id"){// change this value if the primary ID tags are different.
+                        $columnNames[$count] = $post;
+                    }
+                    $count++;
+                }
+            }
+            $table_name = validate_input($_POST['tableSelect'], "Table name");
+            // End of assigning variable to POST values
+            if($errorCount == 0) {
+                $idName = find_ID($table_name);//goes to wLInventory.php
+                $insertOP -> connect();
+                $insertOP -> set_table_names($idName, ...$columnNames);// goes to SQLOp.php
+                $insertOP -> add_query($table_name, ...$columnValues);// goes to SQLOp.php
+                $insertOP -> DB_close();
+                header("Location: inventoryForm.html");
+                exit();
+            }
+            else{
+                echo "No form data received";
+            }
+            break;
 
         // View Table form ***********************************************************************
         case 'form2':
-            $viewOp = new queryOp();// instantiating queryOp class
+            $viewOp = new queryOp(); // Instantiating queryOp class for viewing data
             $table_name = validate_input($_POST['tableView'], $description_k[6]);
             if($errorCount == 0) {
                 $viewOp -> connect();
+                /**
+                 * Sets the table name and queries the table.
+                 * 
+                 * @param string $table_name Name of the table.
+                 */
                 $viewOp -> set_table_name($table_name);
                 $viewOp -> query_table();
                 $viewOp -> print_table();
                 $viewOp -> DB_close();
             }
             else{
-                echo "No form data recieved";
+                echo "No form data received";
             }
             break;
 
-        // operation to update table row ***************************************************************************
+        // Operation to update table row ***************************************************************************
         case 'form3':
-            //local variables
-            $updateOp = new updateOp();
+            // Local variables
+            $updateOp = new updateOp(); // Class object for updating data
             $tableNameValues =[];
             $table_name = validate_input($_POST['tableUpdate'], "update table row");
 
@@ -153,7 +132,7 @@
             }
 
             if(!empty($_POST['wattValue'])){
-                $tableNameValues['wattage'] = validate_input($_POST['wattValue'], "updatting wattage value for table: $table_name");
+                $tableNameValues['wattage'] = validate_input($_POST['wattValue'], "updating wattage value for table: $table_name");
             }
 
             if(!empty($_POST['modValue'])){
@@ -162,26 +141,39 @@
             if($errorCount == 0) {
                 $updateOp -> connect();
                 if(!empty($tableNameValues)){
-                $updateOp -> set_table_update($table_name, $tableNameValues, $p_id_value);}
-                else{echo "no values to update";}
+                    /**
+                     * Sets the table update values.
+                     * 
+                     * @param string $table_name Name of the table.
+                     * @param array $tableNameValues Array of columns and values to update.
+                     * @param int $p_id_value Primary ID value.
+                     */
+                    $updateOp -> set_table_update($table_name, $tableNameValues, $p_id_value);}
+                else{echo "No values to update";}
                 $updateOp -> update_table();
                 $updateOp -> DB_close();
             }
             break;
 
-        // operation to delete table row ***********************************************************************
+        // Operation to delete table row ***********************************************************************
         case 'form4':
-            $deleteOp = new deleteOp(); // instantiating deleteOp class
+            $deleteOp = new deleteOp(); // Instantiating deleteOp class for deleting data
             $table_name = validate_input($_POST['tableDelete'], "deleting row for keyboard table");
             $p_id = validate_input($_POST['pValue'], $description_k[0]);
             if($errorCount == 0) {
                 $deleteOp -> connect();
+                /**
+                 * Sets the table delete values.
+                 * 
+                 * @param string $table_name Name of the table.
+                 * @param int $p_id Primary ID value.
+                 */
                 $deleteOp -> set_table_delete($table_name, $p_id);
                 $deleteOp -> delete_row();
                 $deleteOp -> DB_close();
             }
             break;
-       }
+    }
     ?>
 </body>
 </html>
