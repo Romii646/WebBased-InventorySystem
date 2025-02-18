4f0b3bdc81423fc53dafaa89b6dad97af70ba02c
Romii646 <129922665+Romii646@users.noreply.github.com>
2025-02-17 21:45:04 -0500

    Update wLInventory.php
    Updated file names and file variable names to better reflect on what the file does.

--- wLInventory.php
+++ wLInventory.php
@@ -1,16 +1,21 @@
 <?php
-// Created by Aaron C. 10/08/2024
-   $db = 'database.php';
-   $sSQLS = "selectSQLString.php";
-   $sOP = 'SQLOp.php';
-   $wordList = ["p_id", "name", "condition", "cost", "location", "type", "size", "addMonitorSize", "speed", "wattage", "modular", "addMotherboardSize"];
-   $tableList = ["keyboards", "table"];
-   $description_k = ["key board ID", "computer ID", "key board name", "Condition of part", "Keyboard cost",
-                     "Keyboard location", "input for table name"];
+// Created by Aaron C. 10/08/2024 updated on 02/12/2025
 
+// External file listing
+   $databaseFile = 'database.php';
+   $SQLGeneratorFile = "SQL_statement_generator.php";
+   $SQLOperationFile = 'database_operations.php';
+   $errorFunctionsFile = 'validate_error_functions.php';
 
  if (!function_exists('find_ID')) {
+    /**
+     * Finds the primary key name of the table.
+     * 
+     * @param string $table_name Name of the table.
+     * @return string Primary key name.
+     */
    function find_ID($table_name) {
+    // Array of table names and their corresponding primary key names
        $idArray = [
          "accessories" => "acc_id",
          "graphicscards" => "gpu_id",
